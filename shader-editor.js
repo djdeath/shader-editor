097d40846266f5d968c019cf35952e99cd577a6c
Lionel Landwerlin <llandwerlin@gmail.com>
2013-09-11 16:02:47 +0100

    editor: add ability to select textures for layers

--- shader-editor.js
+++ shader-editor.js
@@ -2,6 +2,7 @@ const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 const Gdk = imports.gi.Gdk;
 const Gtk = imports.gi.Gtk;
+const GnomeDesktop = imports.gi.GnomeDesktop;
 const Clutter = imports.gi.Clutter;
 const GtkClutter = imports.gi.GtkClutter;
 const Cogl = imports.gi.Cogl;
@@ -37,42 +38,115 @@ let parser = new Parser.Parser();
 
 let embeddedStage = new GtkClutter.Embed();
 builder.get_object('live-view-container').add(embeddedStage);
+embeddedStage.show();
 
 let stage = embeddedStage.get_stage();
 stage.background_color = Clutter.Color.get_static(Clutter.StaticColor.BLACK);
 
 let ctx = Clutter.get_default_backend().get_cogl_context();
-let texture = Cogl.Texture.new_from_file(ARGV[0],
-                                         Cogl.TextureFlags.NO_ATLAS,
-                                         Cogl.PixelFormat.ANY);
 
-let pipeline = new Cogl.Pipeline(ctx);
-pipeline.set_layer_texture(0, texture);
-pipeline.set_layer_wrap_mode(0, Cogl.PipelineWrapMode.REPEAT);
+win.show();
+
+/* Pipeline setup */
 
 const PipelineContent = new Lang.Class({
     Name: 'PipelineContent',
     Extends: GObject.Object,
     Implements: [ Clutter.Content, ],
 
-    init: function() {
+    _init: function() {
+        this.parent();
+        this._backPipeline = new Cogl.Pipeline(ctx);
+        this._backPipeline.set_layer_wrap_mode(0, Cogl.PipelineWrapMode.REPEAT);
+        this._backPipeline.set_layer_wrap_mode(1, Cogl.PipelineWrapMode.REPEAT);
+        this._frontPipeline = this._backPipeline;
     },
 
     vfunc_paint_content: function(actor, parent) {
-        let node = new Clutter.PipelineNode(this.pipeline);
+        let node = new Clutter.PipelineNode(this._frontPipeline);
         let box = actor.get_allocation_box();
         node.add_rectangle(box);
         parent.add_child(node);
     },
 
-});
+    setLayerTexture: function(layer, filename) {
+        log('set layer texture ' + layer + ' to ' + filename);
+        this._backPipeline.set_layer_texture(layer,
+                                             Cogl.Texture.new_from_file(filename,
+                                                                        Cogl.TextureFlags.NO_ATLAS,
+                                                                        Cogl.PixelFormat.ANY));
+        this.emit('pipeline-updated');
+    },
 
+    getBackPipeline: function() {
+        return this._backPipeline;
+    },
+
+    setFrontPipeline: function(pipeline) {
+        this._frontPipeline = pipeline;
+        this.invalidate();
+    },
+
+});
+Signals.addSignalMethods(PipelineContent.prototype);
 
 let pipelineContent = new PipelineContent();
-pipelineContent.pipeline = pipeline;
 stage.set_content(pipelineContent);
 
-win.show_all();
+/* Layers edition */
+
+let initializeLayers = function() {
+    let layersTreeView = builder.get_object('layers-treeview');
+    let layersStore = builder.get_object('layers-store');
+
+    let column = new Gtk.TreeViewColumn({ title: 'Name', });
+    let renderer = new Gtk.CellRendererText();
+    column.pack_start(renderer, true);
+    column.add_attribute(renderer, 'text', 2);
+    layersTreeView.append_column(column);
+
+    column = new Gtk.TreeViewColumn({ title: 'Picture', });
+    renderer = new Gtk.CellRendererPixbuf();
+    column.pack_start(renderer, false);
+    column.add_attribute(renderer, 'pixbuf', 0);
+    layersTreeView.append_column(column);
+
+    let layerChooser = builder.get_object('layer-filechooser-dialog');
+    let thumbnailFactory = GnomeDesktop.DesktopThumbnailFactory.new(GnomeDesktop.DesktopThumbnailSize.NORMAL);
+    layerChooser.connect('update-preview', Lang.bind(this, function() {
+        log(layerChooser.preview_widget.width_request);
+        let file = layerChooser.get_file();
+        if (file) {
+            let pixbuf = thumbnailFactory.generate_thumbnail(file.get_uri(), "image/");
+            layerChooser.preview_widget.set_from_pixbuf(pixbuf);
+        }
+    }));
+
+    layerChooser.connect('response', Lang.bind(this, function(dialog, response) {
+        if (response != Gtk.ResponseType.OK) {
+            layerChooser.hide();
+            return;
+        }
+
+        let file = layerChooser.get_file();
+        if (file) {
+            let iter = layersStore.append();
+            let path = layersStore.get_path(iter);
+            let layerId = path.get_indices()[0];
+            layersStore.set(iter, [0, 1, 2],
+                            [layerChooser.preview_widget.pixbuf, file.get_uri(), 'layer' + layerId]);
+            pipelineContent.setLayerTexture(layerId, file.get_path());
+        }
+
+        layerChooser.hide();
+    }));
+
+    builder.get_object('add-layer-button')
+        .connect('clicked', Lang.bind(this, function() {
+            layerChooser.show();
+        }));
+};
+initializeLayers();
 
 /* Error highlight */
 
@@ -164,6 +238,22 @@ gtk_text_view_get_iter_location     (GtkTextView *text_view,
 
 let buffer = builder.get_object('text-buffer');
 let labelResult = builder.get_object('label-result');
+let currentFragmentShader = '';
+
+let updatePipelineShader = function() {
+    if (currentFragmentShader.length > 0) {
+        let newPipeline = pipelineContent.getBackPipeline().copy();
+        newPipeline.add_snippet(new Cogl.Snippet(Cogl.SnippetHook.FRAGMENT, '', currentFragmentShader));
+        pipelineContent.setFrontPipeline(newPipeline);
+    } else {
+        pipelineContent.setFrontPipeline(pipelineContent.getBackPipeline());
+    }
+};
+
+pipelineContent.connect('pipeline-updated', Lang.bind(this, function() {
+    updatePipelineShader();
+}));
+
 
 buffer.connect('changed', Lang.bind(this, function() {
     let str = buffer.get_text(buffer.get_start_iter(),
@@ -194,13 +284,8 @@ buffer.connect('changed', Lang.bind(this, function() {
         // }
 
         /* Setup the new pipeline */
-        let newPipeline = pipeline.copy();
-        newPipeline.add_snippet(new Cogl.Snippet(Cogl.SnippetHook.FRAGMENT,
-                                                 '',
-                                                 str));
-        let content = stage.content;
-        content.pipeline = newPipeline;
-        content.invalidate();
+        currentFragmentShader = str;
+        updatePipelineShader();
     } catch (ex) {
         log('parse failed on : ' + str);
         log(ex);
@@ -213,47 +298,46 @@ buffer.connect('changed', Lang.bind(this, function() {
 /* Inspection handling */
 
 let textView = builder.get_object('fragment-text-view');
-textView.connect('motion-notify-event',
-                 Lang.bind(this, function(widget, event) {
-                     if ((event.get_state()[1] & Gdk.ModifierType.CONTROL_MASK) != 0 &&
-                         parser.yy.literals &&
-                         parser.yy.literals.length > 0) {
-                         let [, x, y] = event.get_coords();
-                         //log(x + 'x' + y);
-                         let iter = textView.get_iter_at_location(x, y);
+textView.connect('motion-notify-event', Lang.bind(this, function(widget, event) {
+    if ((event.get_state()[1] & Gdk.ModifierType.CONTROL_MASK) != 0 &&
+        parser.yy.literals &&
+        parser.yy.literals.length > 0) {
+        let [, x, y] = event.get_coords();
+        //log(x + 'x' + y);
+        let iter = textView.get_iter_at_location(x, y);
 
-                         for (let i in parser.yy.variables) {
-                             let v = parser.yy.variables[i];
+        for (let i in parser.yy.variables) {
+            let v = parser.yy.variables[i];
 
-                             if (v.containsPosition(iter.get_line(), iter.get_line_offset())) {
-                                 highlightElementOnBuffer(buffer, v);
-                                 log('found variable : ' + v.name);
-                                 return false;
-                             }
-                         }
-                         for (let i in parser.yy.functions) {
-                             let f = parser.yy.functions[i];
+            if (v.containsPosition(iter.get_line(), iter.get_line_offset())) {
+                highlightElementOnBuffer(buffer, v);
+                log('found variable : ' + v.name);
+                return false;
+            }
+        }
+        for (let i in parser.yy.functions) {
+            let f = parser.yy.functions[i];
 
-                             if (f.containsPosition(iter.get_line(), iter.get_line_offset())) {
-                                 highlightElementOnBuffer(buffer, f);
-                                 log('found function : ' + f.name);
-                                 return false;
-                             }
-                         }
-                         for (let i in parser.yy.literals) {
-                             let l = parser.yy.literals[i];
+            if (f.containsPosition(iter.get_line(), iter.get_line_offset())) {
+                highlightElementOnBuffer(buffer, f);
+                log('found function : ' + f.name);
+                return false;
+            }
+        }
+        for (let i in parser.yy.literals) {
+            let l = parser.yy.literals[i];
 
-                             if (l.containsPosition(iter.get_line(), iter.get_line_offset())) {
-                                 highlightElementOnBuffer(buffer, l);
-                                 log('found literal : ' + l.value);
-                                 return false;
-                             }
-                         }
-                     }
-                     cleanHighlightFromBuffer(buffer);
+            if (l.containsPosition(iter.get_line(), iter.get_line_offset())) {
+                highlightElementOnBuffer(buffer, l);
+                log('found literal : ' + l.value);
+                return false;
+            }
+        }
+    }
+    cleanHighlightFromBuffer(buffer);
 
-                     return false;
-                 }));
+    return false;
+}));
 
 /* Contextual modifier setup */
 
